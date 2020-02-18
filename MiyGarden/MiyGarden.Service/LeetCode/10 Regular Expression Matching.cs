25e0470dd31b088453cb1393677fa777ce231c10
Clark_Senao <t00121@senao.com.tw>
2020-02-18 17:22:10 +0800

    11

--- MiyGarden/MiyGarden.Service/LeetCode/10 Regular Expression Matching.cs
+++ MiyGarden/MiyGarden.Service/LeetCode/10 Regular Expression Matching.cs
@@ -1,7 +1,5 @@
 ﻿using MiyGarden.Service.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace MiyGarden.Service.LeetCode
 {
@@ -11,145 +9,48 @@ namespace MiyGarden.Service.LeetCode
 
         public void Main()
         {
-            //Console.WriteLine("False=" + this.IsMatch("aa", "a"));
-            //Console.WriteLine("True=" + this.IsMatch("aa", "a*"));
-            //Console.WriteLine("True=" + this.IsMatch("ab", ".b"));
-            //Console.WriteLine("True=" + this.IsMatch("abcdw", ".*"));
-            Console.WriteLine("False=" + this.IsMatch("ab", ".*c"));
-            //Console.WriteLine("True=" + this.IsMatch("abcdw", "a.*"));
-            //Console.WriteLine("True=" + this.IsMatch("abcdwab", ".*ab"));
-            //Console.WriteLine("False=" + this.IsMatch("abcdwab", ".*abc"));
-            //Console.WriteLine("False=" + this.IsMatch("mississippi", "mis*is*p*."));
-            //Console.WriteLine("True=" + this.IsMatch("mississippi", "mis*is*ip*."));
-            //Console.WriteLine("True=" + this.IsMatch("", ""));
-            //Console.WriteLine("False=" + this.IsMatch("a", ""));
-            //Console.WriteLine("False=" + this.IsMatch("", "a"));
-            //Console.WriteLine("True=" + this.IsMatch("", "a*"));
-            //Console.WriteLine("True=" + this.IsMatch("aab", "c*a*b"));
+            Console.WriteLine("False=" + this.IsMatch("aa", "a"));
+            Console.WriteLine("True=" + this.IsMatch("aa", "a*"));
+            Console.WriteLine("True=" + this.IsMatch("ab", ".b"));
+            Console.WriteLine("True=" + this.IsMatch("abcdw", ".*"));
+            Console.WriteLine("True=" + this.IsMatch("abcdw", "a.*"));
+            Console.WriteLine("False=" + this.IsMatch("mississippi", "mis*is*p*."));
+            Console.WriteLine("True=" + this.IsMatch("mississippi", "mis*is*ip*."));
+            Console.WriteLine("True=" + this.IsMatch("", ""));
+            Console.WriteLine("False=" + this.IsMatch("a", ""));
+            Console.WriteLine("False=" + this.IsMatch("", "a"));
+            Console.WriteLine("True=" + this.IsMatch("", "a*"));
+            Console.WriteLine("True=" + this.IsMatch("aab", "c*a*b"));
+            Console.WriteLine("True=" + this.IsMatch("aaa", "a*a"));
+            Console.WriteLine("True=" + this.IsMatch("abcdcbwcb", ".*cb"));
         }
 
+        /// <summary>
+        /// 遞迴分段
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
         public bool IsMatch(string s, string p)
         {
-            var PList = new List<Pattern>();
-            for (var i = 0; i < p.Length; i++)
-            {
-                PList.Add(this.Check(i, p, out int index));
-                i = index;
-            }
+            if (string.IsNullOrEmpty(p))
+                return string.IsNullOrEmpty(s);
 
-            if (string.IsNullOrEmpty(s))
+            if (p.Length > 1 && p[1] == '*')
             {
-                if ((PList.Count == 1 && PList[0].PatternChar == '*') || string.IsNullOrEmpty(p))
-                    return true;
+                if (s.Length > 0 && (s[0] == p[0] || p[0] == '.'))
+                    return this.IsMatch(s.Substring(1), p) || this.IsMatch(s, p.Substring(2));
                 else
-                    return false;
-            }
-
-            return PList.Any(x => x.PatternChar == '*' && x.PrecedingElement == '.') ?
-                this.CheckSp(s, 0, PList) : this.CheckNormal(s, PList);
-        }
-
-        private bool CheckNormal(string s, List<Pattern> pList)
-        {
-            var startIndex = 0;
-            foreach (var item in pList)
-            {
-                if (!item.Check(s, startIndex, out int newIndex))
-                    return false;
-                startIndex = newIndex;
-            }
-
-            return s.Length == startIndex;
-        }
-
-        private bool CheckSp(string s, int index, List<Pattern> pList)
-        {
-            var result = false;
-            for (var j = index; j < s.Length; j++)
-            {
-                var sindex = j;
-                foreach (var item in pList)
-                {
-                    if (!item.Check(s, sindex, out int newIndex))
-                    {
-                        result = false;
-                        break;
-                    }
-                    else
-                        result = true;
-
-                    sindex = newIndex + 1;
-                }
-            }
-
-            return result;
-        }
-
-        private Pattern Check(int index, string p, out int newIndex)
-        {
-            newIndex = index;
-            if (index + 1 >= p.Length)
-                return new Pattern()
-                {
-                    PatternChar = p[index],
-                    CurrentElement = p[index]
-                };
-
-            if (p[index + 1] == '*')
-            {
-                newIndex++;
-                return new Pattern()
-                {
-                    PatternChar = '*',
-                    PrecedingElement = p[index]
-                };
+                    return this.IsMatch(s, p.Substring(2));
             }
             else
-                return new Pattern()
-                {
-                    PatternChar = p[index],
-                    CurrentElement = p[index]
-                };
-        }
-
-        public class Pattern
-        {
-            public char PrecedingElement { get; set; }
-
-            public char CurrentElement { get; set; }
-
-            public char PatternChar { get; set; }
-
-            public bool Check(string s, int startIndex, out int newIndex)
             {
-                bool result;
-                newIndex = startIndex;
-                switch (this.PatternChar)
-                {
-                    case '*':
-                        if (this.PrecedingElement == '.')
-                            newIndex++;
-                        if (s[startIndex] == this.PrecedingElement)
-                            newIndex = GetIndex(startIndex) + 1;
-
-                        return true;
-                    case '.':
-                        newIndex++;
-                        return true;
-                    default:
-                        result = this.CurrentElement == s[startIndex];
-                        if (result)
-                            newIndex++;
-                        return result;
-
-                        int GetIndex(int findStartIndex)
-                        {
-                            if (findStartIndex + 1 >= s.Length || s[findStartIndex + 1] != s[findStartIndex])
-                                return findStartIndex;
-                            else
-                                return GetIndex(findStartIndex + 1);
-                        }
-                }
+                if (string.IsNullOrEmpty(s))
+                    return string.IsNullOrEmpty(p);
+                else if (p[0] == '.' || s[0] == p[0])
+                    return this.IsMatch(s.Substring(1), p.Substring(1));
+                else
+                    return false;
             }
         }
     }
---
+++ MiyGarden/MiyGarden.Service/LeetCode/11 Container With Most Water.cs
@@ -0,0 +1,59 @@
+﻿using MiyGarden.Service.Interfaces;
+using System;
+
+namespace MiyGarden.Service.LeetCode
+{
+    public class _11_Container_With_Most_Water : ILeetCode
+    {
+        public int Number => 11;
+
+        public void Main()
+        {
+            Console.WriteLine("49=" + this.MaxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
+        }
+
+        /// <summary>
+        /// [網路解] 漸進縮小
+        /// https://leetcode.com/problems/container-with-most-water/discuss/6099/yet-another-way-to-see-what-happens-in-the-on-algorithm
+        /// </summary>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public int MaxArea(int[] height)
+        {
+            var maxarea = 0;
+            var l = 0;
+            var r = height.Length - 1;
+            while (l < r)
+            {
+                maxarea = Math.Max(maxarea, Math.Min(height[l], height[r]) * (r - l));
+                if (height[l] < height[r])
+                    l++;
+                else
+                    r--;
+            }
+
+            return maxarea;
+        }
+
+        /// <summary>
+        /// [自解] 迴圈遍歷
+        /// </summary>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public int MaxArea2(int[] height)
+        {
+            var max = 0;
+            for (var i = 0; i < height.Length; i++)
+            {
+                for (var j = i + 1; j < height.Length; j++)
+                {
+                    var area = Math.Min(height[i], height[j]) * Math.Abs(i - j);
+                    if (area > max)
+                        max = area;
+                }
+            }
+
+            return max;
+        }
+    }
+}
