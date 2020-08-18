87405fbdc14fb011d783ffde4d2c73b846617b6f
clark.tsai <clark.tsai@xuenn.com>
2020-08-18 11:10:26 +0800

    tessttt

--- MiyGarden/MiyGarden.WebApi/Controllers/WeatherForecastController.cs
+++ MiyGarden/MiyGarden.WebApi/Controllers/WeatherForecastController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Mime;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 
 namespace MiyGarden.WebApi.Controllers
 {
