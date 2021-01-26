e6821420bc0dd2998e49aebafafbbabd9761871d
clark.tsai <clark.tsai@xuenn.com>
2021-01-26 18:46:21 +0800

    update

---
+++ MiyGarden/MiyGarden.Service/LeetCode/0070 Climbing Stairs.cs
@@ -0,0 +1,100 @@
+﻿using MiyGarden.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiyGarden.Service.LeetCode
+{
+    public class _0070_Climbing_Stairs : ILeetCode
+    {
+        public int Number => 70;
+
+        public void Main()
+        {
+            //Console.WriteLine("2 = " + ClimbStairs(2));
+            //Console.WriteLine("3 = " + ClimbStairs(3));
+            //Console.WriteLine("5 = " + ClimbStairs(4));
+            //Console.WriteLine("8 = " + ClimbStairs(5));
+
+            //Console.WriteLine("8 = " + ClimbStairs(10));
+            //Console.WriteLine("14930352 = " + ClimbStairs(35));
+            //Console.WriteLine("1134903170 = " + ClimbStairs(44));
+
+            Console.WriteLine("2 = " + ClimbStairs2(2));
+            Console.WriteLine("3 = " + ClimbStairs2(3));
+            Console.WriteLine("5 = " + ClimbStairs2(4));
+            Console.WriteLine("8 = " + ClimbStairs2(5));
+
+            Console.WriteLine("8 = " + ClimbStairs2(10));
+            Console.WriteLine("14930352 = " + ClimbStairs2(35));
+            Console.WriteLine("1134903170 = " + ClimbStairs2(44));
+        }
+
+        public int ClimbStairs(int n)
+        {
+            int i;
+            var count = 1;
+            for (i = 1; i <= n / 2; i++)
+            {
+                // i + n - i*2 => n - i
+                var temp = Permutations(n - i, i);
+                count += temp;
+            }
+            return count;
+        }
+
+        private int Permutations(int index, int seed)
+        {
+            var molecular1 = seed;
+            var molecular2 = index - seed;
+            if (molecular1 < molecular2)
+            {
+                var temp = molecular1;
+                molecular1 = molecular2;
+                molecular2 = temp;
+            }
+            int i;
+            ulong result = 1;
+            var dividend = (ulong)GetClass(molecular2);
+            for (i = molecular1 + 1; i <= index; i++)
+            {
+                if (result % dividend == 0)
+                {
+                    result /= dividend;
+                    dividend = 1;
+                }
+                result *= (ulong)i;
+            }
+
+            result /= dividend;
+            return (int)result;
+        }
+
+        private int GetClass(int num)
+        {
+            if (num == 0) return 1;
+            int i;
+            var result = 1;
+            for (i = 2; i <= num; i++)
+                result *= i;
+            return result;
+        }
+
+        public int ClimbStairs2(int n)
+        {
+            return climb_Stairs(0, n);
+        }
+        private int climb_Stairs(int i, int n)
+        {
+            if (i > n)
+            {
+                return 0;
+            }
+            if (i == n)
+            {
+                return 1;
+            }
+            return climb_Stairs(i + 1, n) + climb_Stairs(i + 2, n);
+        }
+    }
+}
---
+++ MiyGarden/MiyGarden.Service/LeetCode/0101 Symmetric Tree.cs
@@ -0,0 +1,35 @@
+﻿using MiyGarden.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiyGarden.Service.LeetCode
+{
+    public class _0101_Symmetric_Tree : ILeetCode
+    {
+        public int Number => 101;
+
+        public void Main()
+        {
+            throw new NotImplementedException();
+        }
+
+        //public bool IsSymmetric(TreeNode root)
+        //{
+
+        //}
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+    }
+}
