8fa9dee431256add709a211a27147590995bddb8
miy123 <kamisamadark@gmail.com>
2023-02-15 18:50:46 +0800

    update

---
+++ MiyGarden/MiyGarden.Service/LeetCode/0104 Maximum Depth of Binary Tree.cs
@@ -0,0 +1,50 @@
+﻿using MiyGarden.Service.Interfaces;
+using System;
+
+namespace MiyGarden.Service.LeetCode
+{
+    public class _0104_Maximum_Depth_of_Binary_Tree : ILeetCode
+    {
+        public int Number => 104;
+
+        public string[] Main()
+        {
+            var result = new string[]
+            {
+            };
+            foreach (var x in result)
+                Console.WriteLine(x);
+            return result;
+        }
+
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+            return GetDepth(root);
+        }
+
+        private int GetDepth(TreeNode root, int depth = 1)
+        {
+            if (root.right == null && root.left == null) return depth;
+            else
+            {
+                var r = root.right == null ? depth : GetDepth(root.right, depth + 1);
+                var l = root.left == null ? depth : GetDepth(root.left, depth + 1);
+                return Math.Max(l, r);
+            }
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+    }
+}
---
+++ MiyGarden/MiyGarden.Service/LeetCode/0118 Pascal's Triangle.cs
@@ -0,0 +1,30 @@
+﻿using MiyGarden.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiyGarden.Service.LeetCode
+{
+    public class _0118_Pascal_s_Triangle : ILeetCode
+    {
+        public int Number => 118;
+
+        public string[] Main()
+        {
+            var result = new string[]
+            {
+            };
+            foreach (var x in result)
+                Console.WriteLine(x);
+            return result;
+        }
+
+        public IList<IList<int>> Generate(int numRows)
+        {
+            if(numRows == 0) return new List
+
+        }
+    }
+}
