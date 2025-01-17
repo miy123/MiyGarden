182968e449fb91fa5d33ada8805b37474206f8e5
miy123 <kamisamadark@gmail.com>
2025-01-17 19:09:20 +0800

    update leetcode

---
+++ MiyGarden/MiyGarden.Service/LeetCode/0074 Search a 2D Matrix.cs
@@ -0,0 +1,64 @@
+﻿using MiyGarden.Service.Interfaces;
+
+namespace MiyGarden.Service.LeetCode
+{
+    /// <summary>
+    /// You are given an m x n integer matrix matrix with the following two properties:
+    /// Each row is sorted in non-decreasing order.
+    /// The first integer of each row is greater than the last integer of the previous row.
+    /// Given an integer target, return true if target is in matrix or false otherwise.
+    /// You must write a solution in O(log(m* n)) time complexity.
+    /// Example 1:
+    /// Input: matrix = [[1, 3, 5, 7], [10,11,16,20], [23,30,34,60]], target = 3
+    /// Output: true
+    /// Example 2:
+    /// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+    /// Output: false
+    /// Constraints:
+    /// m == matrix.length
+    /// n == matrix[i].length
+    /// 1 <= m, n <= 100
+    /// -104 <= matrix[i][j], target <= 104
+    /// </summary>
+    public class _0074_Search_a_2D_Matrix : ILeetCode
+    {
+        public int Number => 74;
+
+        public string[] Main()
+        {
+            return new string[]
+            {
+                "true=" + this.SearchMatrix(new int[][]
+                {
+                    new int[] { 1, 3, 5, 7 },
+                    new int[] { 10, 11, 16, 20 },
+                    new int[] { 23, 30, 34, 60 }
+                }, 3),
+                "false=" + this.SearchMatrix(new int[][]
+                {
+                    new int[] { 1, 3, 5, 7 },
+                    new int[] { 10, 11, 16, 20 },
+                    new int[] { 23, 30, 34, 60 }
+                }, 13)
+            };
+        }
+
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            var m = matrix.Length;
+            var n = matrix[0].Length;
+            var left = 0;
+            var right = m * n - 1;
+            int mid, midValue;
+            while (left <= right)
+            {
+                mid = left + (right - left) / 2;
+                midValue = matrix[mid / n][mid % n];
+                if (midValue == target) return true;
+                else if (midValue < target) left = mid + 1;
+                else right = mid - 1;
+            }
+            return false;
+        }
+    }
+}
---
+++ MiyGarden/MiyGarden.Service/LeetCode/0075 Sort Colors.cs
@@ -0,0 +1,89 @@
+﻿using MiyGarden.Service.Interfaces;
+
+namespace MiyGarden.Service.LeetCode
+{
+    /// <summary>
+    /// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+    /// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+    /// You must solve this problem without using the library's sort function.
+    /// Example 1:
+    /// Input: nums = [2,0,2,1,1,0]
+    ///     Output: [0,0,1,1,2,2]
+    ///     Example 2:
+    /// Input: nums = [2,0,1]
+    ///     Output: [0,1,2]
+    /// Constraints:
+    /// n == nums.length
+    /// 1 <= n <= 300
+    /// nums[i] is either 0, 1, or 2.
+    /// Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+    /// </summary>
+    public class _0075_Sort_Colors : ILeetCode
+    {
+        public int Number => 75;
+
+        public string[] Main()
+        {
+            return new string[]
+            {
+                //"0,0,1,1,2,2=" + string.Join(',',this.SortColors(new int[] { 2, 0, 2, 1, 1, 0 })),
+                //"0,1,2=" + string.Join(',',this.SortColors(new int[] { 2, 0, 1 })),
+                //"0,1=" + string.Join(',',this.SortColors(new int[] { 0, 1 })),
+                //"0,1,2=" + string.Join(',',this.SortColors(new int[] { 0, 2, 1 })),
+                "0,0,0,1,1,1=" + string.Join(',',this.SortColors(new int[] { 0,0,1,0,1,1 })),
+            };
+        }
+
+        public int[] SortColors(int[] nums)
+        {
+            Sort(nums, 0, nums.Length - 1);
+            return nums;
+        }
+
+        private void Sort(int[] nums, int first, int last)
+        {
+            if (first >= last) return;
+            var lastJ = last;
+            var p = nums[first];
+            for (var i = first + 1; i <= lastJ; i++)
+            {
+                if (nums[i] > p)
+                {
+                    for (var j = lastJ; j > i; j--)
+                    {
+                        if (nums[j] < p)
+                        {
+                            (nums[j], nums[i]) = (nums[i], nums[j]);
+                            lastJ = j - 1;
+                            break;
+                        }
+                    }
+                }
+                else if (nums[i] == p)
+                {
+                    for (var j = lastJ; j > i; j--)
+                    {
+                        if (nums[j] < p)
+                        {
+                            (nums[j], nums[i]) = (nums[i], nums[j]);
+                            lastJ = j - 1;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (nums[first] >= nums[lastJ])
+            {
+                (nums[first], nums[lastJ]) = (nums[lastJ], nums[first]);
+                Sort(nums, first, lastJ - 1);
+                Sort(nums, lastJ + 1, last);
+            }
+            else
+            {
+                Sort(nums, first, lastJ - 1);
+                Sort(nums, lastJ, last);
+            }
+        }
+    }
+}
